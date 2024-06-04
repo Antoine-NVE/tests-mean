b1d5a5f2a0d8a143f93536b92da714412375a17a
Antoine-NVE <antoinenavette@gmail.com>
2024-06-04 11:38:46 +0200

    Ajout des autres contrôles front pour l'inscription

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -10,16 +10,20 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
     styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-    registerForm: FormGroup = new FormGroup({
+    public registerForm: FormGroup = new FormGroup({
         pseudo: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
-        email: new FormControl(''),
-        plainPassword: new FormControl(''),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+        plainPassword: new FormControl('', [
+            Validators.required,
+            Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$'),
+            Validators.maxLength(40),
+        ]),
     });
+    public pseudo = this.registerForm.get('pseudo');
+    public email = this.registerForm.get('email');
+    public plainPassword = this.registerForm.get('plainPassword');
 
     public onSubmit() {
-        console.log(this.registerForm.get('pseudo')?.errors);
-
-        console.log(this.registerForm.value);
         if (this.registerForm.valid) {
             console.log('Formulaire soumis avec succès : ');
             console.log(this.registerForm.value);
